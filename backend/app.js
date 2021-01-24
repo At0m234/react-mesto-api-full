6232a7d64c4784bca0711813aff7d0e609835904
At0m234 <61670155+At0m234@users.noreply.github.com>
2021-01-25 01:14:39 +0300

    test9

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ const users = require('./routes/users');
 const cards = require('./routes/cards');
 const NotFoundError = require('./errors/not-found-err');
 // Слушаем 3000 порт
-const { PORT = 3001, BASE__PATH = 'http://localhost:3001/' } = process.env;
+const { PORT = 3000, BASE__PATH = 'http://localhost:3000/' } = process.env;
 
 const app = express();
 
