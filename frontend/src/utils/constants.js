1d6aaf4df8ce842b8239046e23ed5080a8b39b68
At0m234 <61670155+At0m234@users.noreply.github.com>
2021-01-27 17:52:10 +0300

    Проектная работа №15

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,27 +1,3 @@
-// ОБЪЯВЛЕНИЕ ПЕРЕМЕННЫХ - ПОИСК И ДОБАВЛЕНИЕ В DOM
-export const nameInput = document.querySelector(".popup__text_name");
-export const jobInput = document.querySelector(".popup__text_profession");
-export const profileTitle = document.querySelector(".profile__title");
-export const profileProfession = document.querySelector(".profile__profession");
-export const profileImage = document.querySelector(".profile__image");
-export const places = document.querySelector(".places");
-// Переменные попапа редактирования профиля
-export const formEdit = document.forms.edit;
-export const editBtn = document.querySelector(".profile__edit-btn");
-export const EditSaveBtn = formEdit.elements.save;
-export const inputsListFormEdit = Array.from(formEdit.querySelectorAll('.popup__text'));
-// Переменные попапа добавления карточки
-export const formAdd = document.forms.add;
-export const addBtn = document.querySelector(".profile__add-btn");
-export const addCreateBtn = formAdd.elements.create;;
-export const inputsListFormAdd = Array.from(formAdd.querySelectorAll('.popup__text'));
-// Переменные попапа смены аватара пользователя
-export const formAvatar = document.forms.avatar;
-export const editAvatar = document.querySelector(".profile__img-btn");
-export const editAvatarBtn = formAvatar.elements.avatar;
-export const inputsListFormAvatar = Array.from(formAvatar.querySelectorAll('.popup__text'));
-
-
 // Объект с селекторами классов форм
 export const configObj = {
   formSelector: '.popup__container',
