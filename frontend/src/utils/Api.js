0a96316fb666a2654281f61e70952e931bee346b
At0m234 <61670155+At0m234@users.noreply.github.com>
2021-01-30 18:30:55 +0300

    Проектная работа №15(итерация 2)

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -12,80 +12,43 @@ export class Api {
     this._token = token;
   }
   // Приватный метод проверки ответа сервера и преобразование из json
-  // _getResponseData(additionalUrl, optionsObj) {
-  //   return fetch(this._url  + additionalUrl, optionsObj)
-  //     .then(res => {
-  //       if (res.ok) {
-  //         return res.json()
-  //       }
-  //       return Promise.reject(`Ошибка: ${res.status}`);
-  //     })
-  // }
+  _getResponseData(additionalUrl, optionsObj) {
+    return fetch(this._url  + additionalUrl, optionsObj)
+      .then(res => {
+        if (res.ok) {
+          return res.json()
+        }
+        return Promise.reject(`Ошибка: ${res.status}`);
+      })
+  }
 
   // Метод загрузки информации о пользователе с сервера
   getUserInfo() {
-    return fetch(this._url + '/users/me', {
-      headers: {
-        authorization: this._headers
-      }
-    })
-    .then((res) => {
-      if (res.ok) {
-        return res.json();
-      }
-      return Promise.reject(`Что-то пошло не так: ${res.status}`);
-    });
+    return this._getResponseData('/users/me', { headers: this._headers });
   }
 
   // Метод загрузки карточек с сервера
   getCards() {
-    return fetch(this._url + '/cards', {
-      headers: {
-        authorization: this._headers
-      }
-    })
-    .then((res) => {
-      if (res.ok) {
-        return res.json();
-      }
-      return Promise.reject(`Что-то пошло не так: ${res.status}`);
-    });
+    return this._getResponseData('/cards', { headers: this._headers });
   }
 
   // Метод добавления новой карточки на сервер
   addNewCard(formData) {
-    return fetch(this._url + '/cards', {
+    return this._getResponseData('/cards', {
       method: 'POST',
-      headers: {
-        authorization: this._headers,
-        'Content-Type': 'application/json'
-      },
+      headers:  this._headers,
       body: JSON.stringify({
-        name: formData.place,
-        link: formData.url
+        name: formData.title,
+        link: formData.place
       })
     })
-    .then((res) => {
-      if (res.ok) {
-        return res.json();
-      }
-      return Promise.reject(`Что-то пошло не так: ${res.status}`);
-    });
   }
 
   // Метод удаления карточки с сервера
   removeCard(cardId) {
-    return fetch(this._url + '/cards/' + cardId, {
+    return this._getResponseData('/cards/' + cardId, {
       method: 'DELETE',
-      headers: {
-        authorization: this._headers
-      }
+      headers:  this._headers,
     })
-    .then((res) => {
-      if (res.ok) {
-        return res.json();
-      }
-      return Promise.reject(`Что-то пошло не так: ${res.status}`);
-    });
   }
 }
