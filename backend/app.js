d644a049ba3e933b7ac5e431bc166f830d3892c4
At0m234 <61670155+At0m234@users.noreply.github.com>
2021-01-31 18:32:58 +0300

    Проектная работа №15(итерация 4)

--- backend/app.js
+++ backend/app.js
@@ -65,7 +65,7 @@ app.use('/cards', cards);
 
 app.use('/users', users);
 
-app.use('*', (err, req, res, next) => {
+app.use('*', (req, res, next) => {
   res
     .status(404)
     .send({ message: 'Запрашиваемый ресурс не найден' });
