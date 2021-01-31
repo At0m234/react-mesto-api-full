de586e94377a916c20f0be583b350da78a5b4637
At0m234 <61670155+At0m234@users.noreply.github.com>
2021-01-31 15:20:15 +0300

    Проектная работа №15(итерация 4)

--- backend/app.js
+++ backend/app.js
@@ -79,10 +79,17 @@ app.use(errors()); // обработчик ошибок celebrate
 
 // здесь обрабатываем все ошибки
 app.use((err, req, res, next) => {
+  // если у ошибки нет статуса, выставляем 500
+  const { statusCode = 500, message } = err;
+
   res
-    .status(404)
-    .send({ message: err.statusCode });
-  // вызываем next
+    .status(statusCode)
+    .send({
+      // проверяем статус и выставляем сообщение в зависимости от него
+      message: statusCode === 500
+        ? 'На сервере произошла ошибка'
+        : message,
+    });
   next();
 });
 
