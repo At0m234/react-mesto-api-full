3523d5b98cec2cfaa25ba43c005eb937a2c11ad2
At0m234 <61670155+At0m234@users.noreply.github.com>
2021-02-02 13:41:49 +0300

    Проектная работа №15(итерация 5)

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -3,14 +3,14 @@ const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
 const { getCards, createCard, removeCard } = require('../controllers/cards');
 
-const linkRegExp = /^http[s]{0,1}:\/\/[-._~:\/?#[\]@!$&'()*+,;= 0-=a-zA-Z-]*/gm;
+const linkRegExp = "/^http[s]{0,1}:\/\/[-._~:\/?#[\]@!$&'()*+,;= 0-=a-zA-Z-]*/gm";
 
 router.get('/', getCards);
 
 router.post('/', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().regex('linkRegExp').required,
+    link: Joi.string().regex(linkRegExp).required(),
   }),
 }), createCard);
 
