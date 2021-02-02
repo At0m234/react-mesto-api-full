1f33ac7c058d857d112e9f65fe56370cc2587402
At0m234 <61670155+At0m234@users.noreply.github.com>
2021-02-02 13:37:27 +0300

    Проектная работа №15(итерация 5)

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -10,7 +10,7 @@ router.get('/', getCards);
 router.post('/', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().regex(linkRegExp).required,
+    link: Joi.string().regex('linkRegExp').required,
   }),
 }), createCard);
 
