510bc83d1167a72d91ee2a335cc5a0f5b6fead21
At0m234 <61670155+At0m234@users.noreply.github.com>
2021-02-02 14:17:38 +0300

    Проектная работа №15(итерация 5)

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-
+const UnauthorizedError = require('../errors/unauthorized-err');
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
@@ -7,7 +7,8 @@ module.exports = (req, res, next) => {
   const authorization = req.headers.token;
   // убеждаемся, что он есть или начинается с Bearer
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res.status(401).send(JSON.parse("{ message: 'Необходима авторизация' }"));
+    //return res.status(401).send(JSON.parse("{ message: 'Необходима авторизация' }"));
+    next(new UnauthorizedError('Необходима авторизация'));
   }
   // извлечём токен
   const token = authorization.replace('Bearer ', '');
@@ -19,10 +20,9 @@ module.exports = (req, res, next) => {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
   } catch (err) {
     // отправим ошибку, если не получилось
-    return res.status(401).send(JSON.parse("{ message: 'Необходима авторизация' }"));
+    next(new UnauthorizedError('Необходима авторизация'));
+    //return res.status(401).send(JSON.parse("{ message: 'Необходима авторизация' }"));
   }
   req.user = payload; // записываем пейлоуд в объект запроса
   next(); // пропускаем запрос дальше
-
-  return null;
 };
